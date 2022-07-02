a4b47af8d38135fcd4ecb13d14d0c23e0067849d
SimoDedo <dedonatosimone@gmail.com>
2022-07-02 09:27:04 +0200

    veramente un botto di cose
    messo un db dump aggiornati sequence diagram messi i jar per thymeleaf e creato tutte le classi (vuote quasi tutte lmfao)

---
+++ TIW119_HTML_pure/src/main/java/it/polimi/tiw/dao/UserDAO.java
@@ -0,0 +1,98 @@
+package it.polimi.tiw.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import it.polimi.tiw.beans.User;
+
+public class UserDAO {
+    private Connection con;
+
+	public UserDAO(Connection connection) {
+		this.con = connection;
+	}
+
+	public User checkCredentials(String username, String password) throws SQLException {
+		String query = "SELECT  id, email, username, name, surname FROM user  WHERE username = ? AND password =?";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, username);
+			pstatement.setString(2, password);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setID(result.getInt("id"));
+                    user.setEmail(result.getString("email"));
+					user.setUsername(result.getString("username"));
+					user.setName(result.getString("name"));
+					user.setSurname(result.getString("surname"));
+					return user;
+				}
+			}
+		}
+	}
+
+    public User getUserByUsername(String username) throws SQLException{
+        String query = "SELECT  id, email, username, name, surname FROM user  WHERE username = ?";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, username);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setID(result.getInt("id"));
+                    user.setEmail(result.getString("email"));
+					user.setUsername(result.getString("username"));
+					user.setName(result.getString("name"));
+					user.setSurname(result.getString("surname"));
+					return user;
+				}
+			}
+        }
+    }
+
+    public User getUserByEmail(String email) throws SQLException{
+        String query = "SELECT  id, email, username, name, surname FROM user  WHERE username = ?";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, email);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setID(result.getInt("id"));
+                    user.setEmail(result.getString("email"));
+					user.setUsername(result.getString("username"));
+					user.setName(result.getString("name"));
+					user.setSurname(result.getString("surname"));
+					return user;
+				}
+			}
+        }
+    }
+
+    public void registerUser(String name,String  surname,String  username,String  email,String  password) throws SQLException{
+        String query = "INSERT into user (email, username, password, name, surname) VALUES (?, ?, ?, ?, ?)";
+        con.setAutoCommit(false);
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, email);
+			pstatement.setString(2, username);
+			pstatement.setString(3, password);
+			pstatement.setString(4, name);
+			pstatement.setString(5, surname);
+            pstatement.executeUpdate();
+            con.commit();
+        }
+        catch(SQLException e){
+            con.rollback();
+            throw e;
+        }
+        
+    }
+}
---
+++ db dumps/Dump20220702.sql
@@ -0,0 +1,145 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: tiw119
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `balance` decimal(12,2) NOT NULL,
+  `userid` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `userid_idx` (`userid`),
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (7,'contodisimo',1000.00,1),(8,'contodivale',2000.00,2),(9,'contodiuser',3000.50,3);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact` (
+  `userid` int NOT NULL,
+  `accountid` int NOT NULL,
+  PRIMARY KEY (`userid`,`accountid`),
+  KEY `contactid_idx` (`accountid`),
+  CONSTRAINT `contactid` FOREIGN KEY (`accountid`) REFERENCES `account` (`id`),
+  CONSTRAINT `ownerid` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,7),(1,8),(3,8),(1,9),(2,9);
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movement`
+--
+
+DROP TABLE IF EXISTS `movement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movement` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL,
+  `amount` decimal(12,2) NOT NULL,
+  `motive` varchar(45) NOT NULL,
+  `inAccountID` int NOT NULL,
+  `outAccountID` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `inAccountid_idx` (`inAccountID`),
+  KEY `outAccountid_idx` (`outAccountID`),
+  CONSTRAINT `inAccountid` FOREIGN KEY (`inAccountID`) REFERENCES `account` (`id`),
+  CONSTRAINT `outAccountid` FOREIGN KEY (`outAccountID`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movement`
+--
+
+LOCK TABLES `movement` WRITE;
+/*!40000 ALTER TABLE `movement` DISABLE KEYS */;
+INSERT INTO `movement` VALUES (2,'2020-07-02 07:10:32',50.00,'per provare',7,8);
+/*!40000 ALTER TABLE `movement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Simo','simo@mail.it','simo','Simone','de Donato'),(2,'Vale','vale@mail.it','vale','Valerio','Donno'),(3,'User','user@mail.it','user','User','User');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-02  9:23:23
