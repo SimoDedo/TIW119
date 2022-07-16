91f672dbf99ca58303893f3bdedf3eb853bf4608
SimoDedo <dedonatosimone@gmail.com>
2022-07-16 18:37:05 +0200

    Tolta la possibilità di chiamare con una get le servlet di solo post
    era lì per ragioni storiche

--- TIW119_HTML_pure/src/main/java/it/polimi/tiw/controllers/CheckLogin.java
+++ TIW119_HTML_pure/src/main/java/it/polimi/tiw/controllers/CheckLogin.java
@@ -37,13 +37,6 @@ public class CheckLogin extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- TIW119_HTML_pure/src/main/java/it/polimi/tiw/controllers/CheckSignup.java
+++ TIW119_HTML_pure/src/main/java/it/polimi/tiw/controllers/CheckSignup.java
@@ -39,13 +39,6 @@ public class CheckSignup extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
